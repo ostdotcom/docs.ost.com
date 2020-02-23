67fd8803c684a552e749909a0e8ad492befb4890
kevinfs510 <kevin@ost.com>
2020-02-23 17:41:39 +0100

    antora docs initial setup

---
+++ ui/src/scripts/vendor/highlight.js
@@ -0,0 +1,14 @@
+;(function () {
+  'use strict'
+
+  var hljs = require('highlight.js/lib/highlight');
+  hljs.registerLanguage('javascript', require('highlight.js/lib/languages/javascript'));
+  hljs.registerLanguage('typescript', require('highlight.js/lib/languages/typescript'));
+  hljs.registerLanguage('json', require('highlight.js/lib/languages/json'));
+  hljs.registerLanguage('diff', require('highlight.js/lib/languages/diff'));
+  hljs.registerLanguage('python', require('highlight.js/lib/languages/python'));
+  hljs.registerLanguage('shell', require('highlight.js/lib/languages/shell'));
+  hljs.registerLanguage('xml', require('highlight.js/lib/languages/xml'));
+  require('highlightjs-solidity')(hljs);
+  hljs.initHighlightingOnLoad();
+})();
